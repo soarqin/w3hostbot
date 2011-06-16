00b4ff0c3067b9c896388761b4389718b68f911c
Soar Qin <soarchin@verycd.com>
2011-06-16 12:01:25 +0800

    fix

--- plugins/gametcp/TcpMapSize.cpp
+++ plugins/gametcp/TcpMapSize.cpp
@@ -19,16 +19,17 @@ namespace Plugins
 			if(!player.SetMapProgress(completed) && completed == 0 && player.GetHost() != NULL)
 			{
 				Common::Stream st2;
-				st2<<mapid<<(byte)1;
+				byte pid = player.GetPID();
+				byte tid = pid == 1 ? 2 : 1;
+				st2<<mapid<<tid;
 				player.Send(0x3FF7, st2);
 
-				byte pid = player.GetPID();
 				HostBot::MapCfg::Pointer cfg = player.GetHost()->GetMapCfg();
 				std::vector<uint>& crclist = cfg->GetCrcList();
 				for(size_t idx = 0, pos = 0; pos < cfg->GetSize(); pos += 1442, ++ idx)
 				{
 					Common::Stream st;
-					st<<(byte)pid<<(byte)1<<mapid<<pos;
+					st<<(byte)pid<<tid<<mapid<<pos;
 					uint sz = 1442;
 					if(pos + sz > cfg->GetSize())
 						sz = cfg->GetSize() - pos;
